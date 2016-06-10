ffa47326eba53ac4f918e4c20e68468a9d0db3ad
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-06-11 08:01:40 +0900

    Submit to PyPI
    Fixes #4
    Thanks to Robey Pointer for permission to upload to PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+'''
+Growl when a command is finished.
+
+Original version by Robey Pointer https://github.com/robey
+'''
+
+
 from setuptools import setup
 
 setup(
     name='growlme',
     version='0.2',
-    description='Growl when a comand is finished',
+    description=__doc__,
+    url='https://github.com/kfdm/growlme',
+    maintainer='Paul Traylor',
     install_requires=['gntp'],
     license='Apache',
     packages=['growlme'],
